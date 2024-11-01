4dd1ece26b0d51c285eb4f8409a25ca32ffbc52e
Rizwan Afzal <Rizwan.Afzal@salik.ae>
2024-11-01 23:40:50 +0400

    Added Fluent Validation libary for validations

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,11 +4,24 @@
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
    : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler (IDocumentSession session)
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand> 
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+
+        }
+    }
+    internal class CreateProductCommandHandler (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@command}", command);
             var product = new Product //Create Product entity from command object
             {
                 Name = command.Name,
